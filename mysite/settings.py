c6bfc1a8bf61e31c77e3baae639d826c340c364a
Giancarlo Portillo <gtp@hawaii.edu>
2022-06-19 17:08:06 -1000

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ[''],
+        'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
